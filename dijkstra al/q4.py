89a899c1172dd9073aac566a100afcee49bd98fd
hyeomjeong <hyeom1027@gmail.com>
2021-06-12 23:45:30 +0900

    [수정] dijkstra 문제 해결 - 예외처리

--- dijkstra al/q4.py
+++ dijkstra al/q4.py
@@ -24,20 +24,27 @@ if __name__ == '__main__':
     for _ in range(T):
         n, m, t = map(int, stdin.readline().split()) # t 中 1
         s, g, h = map(int, stdin.readline().split()) # s = start
-        adj_list = {x: {} for x in range(1, m+1)}
+        adj_list = {x: {} for x in range(1, n + 1)}
+        road = 0
         for _ in range(m):
             a, b, d = map(int, stdin.readline().split())
+            if (a == g and b == h) or (a == h and b == g):
+                road = d
             adj_list[a][b] = d if b not in adj_list[a].keys() else min(d, adj_list[a][b])
             adj_list[b][a] = d if a not in adj_list[b].keys() else min(d, adj_list[b][a])
 
         s_distances = dijkstra(adj_list, s)
         g_distances = dijkstra(adj_list, g)
         h_distances = dijkstra(adj_list, h)
-
+        #print(g_distances, h_distances)
         t_list = [int(stdin.readline().rstrip()) for _ in range(t)]
+        result = []
         for tmp in t_list:
-            if g == t:
-                if
-
-            if  == s_distances[tmp]
-
+            if s_distances[tmp] == float('inf') or g_distances[tmp] == float('inf') or h_distances[tmp] == float('inf'):
+                continue
+            gh_total = s_distances[g] + road + h_distances[tmp]
+            hg_total = s_distances[h] + road + g_distances[tmp]
+            if s_distances[tmp] == gh_total or s_distances[tmp] == hg_total:
+                result.append(tmp)
+        result.sort()
+        print(" ".join(map(str, result)))
